ce2390c59e8d2892c2fcf8451a944f9aaa23efeb
blawesom <blaplane@gmail.com>
2017-01-18 17:25:30 +0100

    Base commit

---
+++ api_caller.py
@@ -0,0 +1,65 @@
+# /usr/local/bin/python
+# encoding = utf-8
+
+import sys, os, re
+import sigv4
+import requests
+import inspect
+
+
+def call_api(method, endpoint, action, payload='', path='/'):
+    """
+    Generate a signed call to the API to return status and content
+
+    :param method: GET or POST method verb
+    :type method: str
+    :param endpoint: endpoint of the api for the service (fcu.eu-west-2.outscale.com)
+    :type endpoint: str
+    :param action: REST formated action (see docs.outscale.com)
+    :type action: str
+    :param payload: payload for POST requests
+    :type payload: str
+    :param path: path of the API
+    :type path: str
+    :return: status_code and content
+    :rtype: tuple
+    """
+
+    access_key = os.environ.get('AWS_ACCESS_KEY_ID')
+    secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+    if access_key is None or secret_key is None:
+        print 'No access key is available.'
+        sys.exit()
+
+    services = {'fcu': 'ec2',
+                'lbu': 'elb',
+                'osu': 's3',
+                'eim': 'iam'}
+
+    request_parameters = '{}&Version=2016-10-15'.format(action)
+
+    params = { 'method': method,
+                'service': services[endpoint.split('.')[0]],
+                'host': endpoint,
+                'request_parameters': request_parameters,
+                'payload': payload,
+                'path': path,
+                'access_key': access_key,
+                'secret_key': secret_key }
+
+    signed_request, headers = sigv4.sign_request(params)
+
+    endpoint = 'https://{}'.format(params['host'])
+    if method == 'GET':
+        request_url = endpoint + '?' + signed_request
+        r = requests.get(request_url, headers=headers)
+    elif method == 'POST':
+        r = requests.post(endpoint, data=signed_request, headers=headers)
+    else:
+        return 0, 'Method not supported'
+    # Clean XML for proper parsing
+    pattern = "( xmlns\=\"[:.a-zA-Z0-9\/-]*\")"
+    req = re.sub(pattern, '', r.text)
+
+    return r.status_code, req
---
+++ sigv4.py
@@ -0,0 +1,48 @@
+# /usr/local/bin/python
+# encoding = utf-8
+
+import sys, os, base64, datetime, hashlib, hmac, re
+
+
+def sign(key, msg):
+    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+
+
+def getSignatureKey(key, dateStamp, regionName, serviceName):
+    kDate = sign(('AWS4' + key).encode('utf-8'), dateStamp)
+    kRegion = sign(kDate, regionName)
+    kService = sign(kRegion, serviceName)
+    kSigning = sign(kService, 'aws4_request')
+    return kSigning
+
+
+def sign_request(params):
+    region = params['host'].split('.')[1]
+    endpoint = 'https://{}'.format(params['host'])
+
+    # Create a date for headers and the credential string
+    t = datetime.datetime.utcnow()
+    amzdate = t.strftime('%Y%m%dT%H%M%SZ')
+    datestamp = t.strftime('%Y%m%d') # Date w/o time, used in credential scope
+    canonical_uri = params['path']
+
+    # Query string values must be URL-encoded (space=%20).
+    canonical_querystring = params['request_parameters']
+    canonical_headers = 'host:' + params['host'] + '\n' + 'x-amz-date:' + amzdate + '\n'
+    signed_headers = 'host;x-amz-date'
+    payload_hash = hashlib.sha256(params['payload']).hexdigest()
+    canonical_request = params['method'] + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
+
+    algorithm = 'AWS4-HMAC-SHA256'
+    credential_scope = datestamp + '/' + region + '/' + params['service'] + '/' + 'aws4_request'
+    string_to_sign = algorithm + '\n' +  amzdate + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request).hexdigest()
+    signing_key = getSignatureKey(params['secret_key'], datestamp, region, params['service'])
+
+    # Sign the string_to_sign using the signing_key
+    signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
+
+    # Create authorization header and add to request headers
+    authorization_header = algorithm + ' ' + 'Credential=' + params['access_key'] + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
+    headers = {'x-amz-date':amzdate, 'Authorization':authorization_header}
+
+    return canonical_querystring, headers
