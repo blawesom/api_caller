1112d5295856b7aa9c2fb8250962545c63463065
blawesom <blaplane@gmail.com>
2017-03-10 09:01:45 +0100

    POSIX argument handling (simpler)

--- ows_cli.py
+++ ows_cli.py
@@ -21,28 +21,16 @@ def call(call_arg):
     return status_code, response
 
 
-def args_to_string(dict_args):
-    request = ''
-    for arg in dict_args:
-        joined = '='.join([arg, dict_args[arg]])
-        request = '{}&{}'.format(request, joined)
-    # Action=MyAction&param1=value1&param2=value2&tags.keyn=valuen&tags.keym=valuem
-    return request[1:]
-
-
 if __name__ == '__main__':
     print ''
-    parser = argparse.ArgumentParser()
-    args, ukargs = parser.parse_known_args()
+    arg_list = []
+    for arg in sys.argv[:1]
+        if arg startswith('--'):
+            arg_list.append(arg[2:])
+    parsed_string = '&'.joint(arg_list)
+    # Action=MyAction&param1=value1&param2=value2&tags.keyn=valuen&tags.keym=valuem
 
-    parsed = {}
-    for i, arg in enumerate(ukargs):
-        if arg.startswith('--'):
-            parsed[arg[2:]] = ukargs[i+1]
-        else:
-            pass
-            
-    status_code, response = call(args_to_string(parsed))
+    status_code, response = call(parsed_string)
 
     print "Code: {}".format(status_code)
     print "Request Id: {}".format(response.find('requestId').text)
